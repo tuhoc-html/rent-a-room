6222e583ffa12aa4798da2cafb9eff1d855ce770
thanh <thanhleX04@gmail.com>
2025-12-14 23:14:22 +0700

    exception

--- room_rental_be/src/main/java/com/chronosx/room_rental_be/infrastructure/exception/ForbiddenException.java
+++ room_rental_be/src/main/java/com/chronosx/room_rental_be/infrastructure/exception/ForbiddenException.java
@@ -1,11 +1,11 @@
 package com.chronosx.room_rental_be.infrastructure.exception;
 
 public class ForbiddenException extends AppException {
-	public ForbiddenException(String message) {
-		super(ErrorCode.FORBIDDEN, message);
-	}
+    public ForbiddenException(String message) {
+        super(ErrorCode.FORBIDDEN, message);
+    }
 
-	public ForbiddenException(ErrorCode errorCode, String message) {
-		super(errorCode, message);
-	}
+    public ForbiddenException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
 }
